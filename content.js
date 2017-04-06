ec99562e654b497d881defc5d4fc0cf38c587526
William Jeffries <wm.jeffries@gmail.com>
2017-04-06 13:44:02 -0400

    Add pasting within textarea fields
    Signed-off-by: Elle Mundy <elle@bauerxcel.com>

---
+++ content.js
@@ -0,0 +1,26 @@
+function insertTextAtCursor(el, text) {
+  var val = el.value, endIndex, range;
+  if (typeof el.selectionStart != "undefined" && typeof el.selectionEnd != "undefined") {
+    endIndex = el.selectionEnd;
+    el.value = val.slice(0, el.selectionStart) + text + val.slice(endIndex);
+    el.selectionStart = el.selectionEnd = endIndex + text.length;
+  } else if (typeof document.selection != "undefined" && typeof document.selection.createRange != "undefined") {
+    el.focus();
+    range = document.selection.createRange();
+    range.collapse(false);
+    range.text = text;
+    range.select();
+  }
+}
+
+
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if( request.message === "paste_link" ) {
+    var el = document.activeElement;
+    if (el.type == "textarea") {
+      insertTextAtCursor(el,"http://www.sweepon.com/sweepstakes/one-million-dollar-giveaway-13779");
+    }
+  }
+}
+                                    );
